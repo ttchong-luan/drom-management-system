c5286fa361b2cf7e6f7b07939b13408c3a51506f
李子祥 <629075579@qq.com>
2026-01-30 13:16:53 +0800

    Update and rename 哦呢.py to one.py

---
+++ templates/one.py
@@ -0,0 +1,4 @@
+Python代码查看版本
+import sqlalchemy
+print("SQLAlchemy 版本：", sqlalchemy.__version__)
+
--- templates/哦呢.py
+++
@@ -1,7 +0,0 @@
-# 方式1：Python代码查看版本
-import sqlalchemy
-print("SQLAlchemy 版本：", sqlalchemy.__version__)
-
-# 方式2：终端命令查看（推荐）
-# pip list | grep sqlalchemy  # Linux/Mac
-# pip list | findstr sqlalchemy  # Windows
